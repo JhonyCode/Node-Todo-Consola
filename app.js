4433742a2d3d9850502cce13e529ae63b16a2e1a
Jonathan <jhonyganehd@gmail.com>
2024-06-05 12:58:19 +0200

    Crear tareas en memoria

--- app.js
+++ app.js
@@ -1,8 +1,31 @@
+const { inquirerMenu, pausa, leerInput } = require('./helpers/inquirer');
+const Tarea = require('./models/tarea');
+const Tareas = require('./models/tareas');
 require('colors');
-console.clear();
 
 const main = async() => {
     console.log('Hola Mundo');
+
+    let opt = '';
+    const tareas = new Tareas();
+    do {
+       opt = await inquirerMenu();
+
+       switch (opt) {
+        case '1':
+            const desc = await leerInput('Descripción:');
+            tareas.crearTarea(desc);
+            break;
+        case '2':
+            console.log(tareas._listado);
+            break;
+       }
+
+
+
+       await pausa();
+        
+    } while (opt !== '0');
 }
 
 main();
